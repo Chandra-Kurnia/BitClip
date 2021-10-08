5e6d209a44bc7cf95d9d4eea2bf2311929fcb9f3
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-10-08 19:05:26 +0700

    fix error loader

---
+++ src/Components/base/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from '../../assets/loading.gif'
+
+const Loading = () => {
+    return (
+        <div className='loader'>
+            <img src={Loader} alt="" />
+        </div>
+    )
+}
+
+export default Loading
